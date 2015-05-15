dc95ee2cb1954298f513d30d5e97c00fbcec3b61
Xie Chenhong <chenhong.xie@gmail.com>
2015-05-16 03:22:58 +0800

    Tab clean up

--- nsmf.js
+++ nsmf.js
@@ -1,39 +1,39 @@
 $(function() {
-	var UsmfEngine = function() {
-		var rendered = "",
-			tpl_dict = {};
+    var UsmfEngine = function() {
+        var rendered = "",
+            tpl_dict = {};
 
-		var render = function(tpl, data, elem) {
-			tpl = window.location.href + tpl;
-			if (tpl_dict[tpl] === "") {
-				$.get(
-					tpl,
-					function(resp) {
-						tpl_dict[tpl] = _.template(resp);
-						elem.html(tpl_dict[tpl](data));
-						// console.log("Evaled:", tpl);
-					}
-				);
-			} else {
-				console.log("Why templates are never cached?!");
-				elem.html(tpl_dict[tpl](data));
-			}
-		};
+        var render = function(tpl, data, elem) {
+            tpl = window.location.href + tpl;
+            if (tpl_dict[tpl] === "") {
+                $.get(
+                    tpl,
+                    function(resp) {
+                        tpl_dict[tpl] = _.template(resp);
+                        elem.html(tpl_dict[tpl](data));
+                        // console.log("Evaled:", tpl);
+                    }
+                );
+            } else {
+                console.log("Why templates are never cached?!");
+                elem.html(tpl_dict[tpl](data));
+            }
+        };
 
-		_.each(
-			$("script[type=template]"),
-			function(x) { tpl_dict[x.src] = ""; }
-		);
-		return render;
-	};
+        _.each(
+            $("script[type=template]"),
+            function(x) { tpl_dict[x.src] = ""; }
+        );
+        return render;
+    };
 
-	var data1 = {text: "Oh, hi!"};
-	var data2 = {text: "Oh, hello!"};
-	var data3 = {text: "Oh, this is awesome!"};
+    var data1 = {text: "Oh, hi!"};
+    var data2 = {text: "Oh, hello!"};
+    var data3 = {text: "Oh, this is awesome!"};
 
-	var ue = UsmfEngine();
+    var ue = UsmfEngine();
 
-	ue("tpl1.html", data1, $("#tpl-test1"));
-	ue("tpl2.html", data2, $("#tpl-test2"));
-	ue("tpl1.html", data3, $("#tpl-test3"));
+    ue("tpl1.html", data1, $("#tpl-test1"));
+    ue("tpl2.html", data2, $("#tpl-test2"));
+    ue("tpl1.html", data3, $("#tpl-test3"));
 });
